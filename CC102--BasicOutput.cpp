a787e7d3c744f4be75b833deb404678983f618e3
agazaCS <jeremyagaza@gmail.com>
2024-10-04 09:50:05 +0800

    Add files via upload

---
+++ CC102--BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+    cout << "Our Lady of Lourdes College Foundation" <<endl;
+	cout << "College of Computer Studies" <<endl;
+	cout << "Bachelor of Science in Computer Science-1st Year" <<endl;
+	cout << "Jeremy Agaza" <<endl;
+	cout << "Purok-3,Sta cruz,Talisay,Camarines Norte" <<endl;
+	return 0;
+}
