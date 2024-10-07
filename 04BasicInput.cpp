93dfa124a81c193d7f0878f086fa4714580af1be
Jeremy Agaza <jeremyagaza@gmail.com>
2024-10-07 09:46:06 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()l
+	string name;
+	int Course, School;
+	cout << "Nickname:";
+	cin >> name; 
+	cout << "Course-Year Level:";
+	cin >> Course;
+	cout << "School:";
+	cout >>"                             \n";
+	
+	cout << "Wow Congrats" << Nickname << "! " << courseInYear<< " is a nice course and you are studying<<'\n";
+	cout << "in" << school << "which is one of the Center of exellence in teriary Education"<<endl:
+	return 0;
+
+}
+	
